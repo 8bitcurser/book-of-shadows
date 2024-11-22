125348fcd05d30e26944cca5a48af61f7892509a
Tom <tomowl@pm.me>
2024-11-21 23:56:07 -0300

    Creator method for investigator

--- main.go
+++ main.go
@@ -111,22 +111,3 @@ func (*Investigator) NewInvestigator() Investigator {
 		DamageBonus:      "1D4",
 	}
 }
-
-//func handleConnection(w http.ResponseWriter, r *http.Request) {
-//
-//	// Log the path that was requested
-//	fmt.Printf("Method: %s\nPath: %s\n", r.Method, r.URL.Path)
-//
-//	// Send response
-//	w.Header().Set("Content-Type", "application/json")
-//	w.WriteHeader(http.StatusOK)
-//	json.NewEncoder(w).Encode(map[string]string{"message": "Got it"})
-//}
-//
-//func main() {
-//	fmt.Println("Server listening on :8080")
-//	http.HandleFunc("/", handleConnection)
-//	if err := http.ListenAndServe(":8080", nil); err != nil {
-//		panic(err)
-//	}
-//}
