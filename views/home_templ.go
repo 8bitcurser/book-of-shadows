e4e3e5855bab66fb7716ad260e37eb0a94321b0f
Tom <tomowl@pm.me>
2025-05-19 11:42:41 -0300

    Re build go templates

--- views/home_templ.go
+++ views/home_templ.go
@@ -8,7 +8,7 @@ package views
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-import "yourproject/components"
+import "book-of-shadows/components"
 
 func Home() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
