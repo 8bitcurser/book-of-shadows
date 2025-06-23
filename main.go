8d4ff112039d0da2fa3406e77d181c588ff15309
Tom <tomowl@pm.me>
2025-06-23 00:22:37 -0300

    Implement handmade router to fix api deisng

--- main.go
+++ main.go
@@ -2,62 +2,18 @@ package main
 
 import (
 	"book-of-shadows/storage"
+	"fmt"
 	"log"
 	"net/http"
 )
 
-type Route struct {
-	handlers map[string]http.HandlerFunc
-}
-
-type Router struct {
-	routes map[string]*Route
-}
-
-func NewRouter() *Router {
-	return &Router{
-		routes: make(map[string]*Route),
-	}
-}
-
-func (r *Router) Handle(method, path string, handler http.HandlerFunc) {
-	if r.routes[path] == nil {
-		r.routes[path] = &Route{
-			handlers: make(map[string]http.HandlerFunc),
-		}
-	}
-	r.routes[path].handlers[method] = handler
-}
-
-func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	if route, exists := r.routes[req.URL.Path]; exists {
-		if handler, methodExists := route.handlers[req.Method]; methodExists {
-			handler(w, req)
-			return
-		}
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-	}
-	http.NotFound(w, req)
-}
-
-func (r *Router) GET(path string, handler http.HandlerFunc) {
-	r.Handle(http.MethodGet, path, handler)
-}
-
-func (r *Router) POST(path string, handler http.HandlerFunc) {
-	r.Handle(http.MethodPost, path, handler)
-}
-
-func (r *Router) PUT(path string, handler http.HandlerFunc) {
-	r.Handle(http.MethodPut, path, handler)
-}
-
-func (r *Router) DELETE(path string, handler http.HandlerFunc) {
-	r.Handle(http.MethodDelete, path, handler)
-}
-
 func setupRoutes() *Router {
 	router := NewRouter()
+
+	fs := http.FileServer(http.Dir("static"))
+	fsHandler := http.StripPrefix("/static/", fs)
+	router.HandleStatic("/static/", fsHandler)
+	fmt.Printf("Static handlers: %+v\n", router.statics)
 
 	// Define routes
 	router.GET("/", handleHome)
@@ -105,8 +61,7 @@ func main() {
 	// http.HandleFunc("/api/investigator/", handleGetInvestigator)
 	// http.HandleFunc("/api/archetype/", handleArchetypeOccupations)
 	// http.HandleFunc("/api/report-issue", handleReportIssue)
-	fileServer := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
 	log.Println("Server starting on :8080")
 	if err := http.ListenAndServe(":8080", router); err != nil {
 		log.Fatal(err)
---
+++ routers.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+)
+
+type Route struct {
+	handlers map[string]http.HandlerFunc
+}
+
+type StaticRoute struct {
+	handlers map[string]http.Handler
+}
+
+type Router struct {
+	routes  map[string]*Route
+	statics map[string]*StaticRoute
+}
+
+func NewRouter() *Router {
+	return &Router{
+		routes:  make(map[string]*Route),
+		statics: make(map[string]*StaticRoute),
+	}
+}
+
+func (r *Router) Handle(method, path string, handler http.HandlerFunc) {
+	if r.routes[path] == nil {
+		r.routes[path] = &Route{
+			handlers: make(map[string]http.HandlerFunc),
+		}
+	}
+	r.routes[path].handlers[method] = handler
+}
+
+func (r *Router) HandleStatic(path string, handler http.Handler) {
+	if r.statics[path] == nil {
+		r.statics[path] = &StaticRoute{
+			handlers: make(map[string]http.Handler),
+		}
+	}
+	r.statics[path].handlers[http.MethodGet] = handler
+}
+
+func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+
+	if strings.HasPrefix(req.URL.Path, "/static/") {
+		handler := r.statics["/static/"].handlers[http.MethodGet]
+		handler.ServeHTTP(w, req)
+		return
+	} else {
+		if route, exists := r.routes[req.URL.Path]; exists {
+			if handler, methodExists := route.handlers[req.Method]; methodExists {
+				handler(w, req)
+				return
+			}
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		for path, route := range r.routes {
+			if strings.HasSuffix(path, "/") && strings.HasPrefix(req.URL.Path, "/static/") {
+				if handler, methodExists := route.handlers[req.Method]; methodExists {
+					handler(w, req)
+					return
+				}
+				http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+				return
+			}
+		}
+	}
+
+	http.NotFound(w, req)
+}
+
+func (r *Router) GET(path string, handler http.HandlerFunc) {
+	r.Handle(http.MethodGet, path, handler)
+}
+
+func (r *Router) POST(path string, handler http.HandlerFunc) {
+	r.Handle(http.MethodPost, path, handler)
+}
+
+func (r *Router) PUT(path string, handler http.HandlerFunc) {
+	r.Handle(http.MethodPut, path, handler)
+}
+
+func (r *Router) DELETE(path string, handler http.HandlerFunc) {
+	r.Handle(http.MethodDelete, path, handler)
+}
